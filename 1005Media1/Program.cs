83e71e27d8e4ba753a8680e64b111b2941039713
Paola <Paola@DESKTOP-L53JRFL>
2024-04-23 22:13:23 -0300

    Adicionar arquivos de projeto.

---
+++ 1005Media1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+
+        double A, B, MEDIA;
+
+        A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        MEDIA = ((A*3.5) + (B*7.5))/11;
+
+        Console.WriteLine("MEDIA = " + MEDIA.ToString("F5", CultureInfo.InvariantCulture));
+
+        Console.Read();
+
+
+    }
+
+}
